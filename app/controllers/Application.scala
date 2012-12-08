7ccb2d3534127043714239ec265fb8c7f7ff75de
Gustavo Recio <gustavo.recio@spindriftgroup.com>
2012-12-08 13:33:10 +0000

    Adding contacts

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,13 +2,31 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+import models.Contact
 
 object Application extends Controller {
   
   def list = Action {
-    Ok(views.html.index("Listing contacts."))
+    Ok(views.html.index(Contact.allContacts(), contactForm))
   }
 
   def getContactDetails(name: String) = TODO
   
+
+  val contactForm = Form(
+	  "name" -> nonEmptyText
+  )
+
+  def newContact = Action { implicit request =>
+	  contactForm.bindFromRequest.fold(
+	    errors => BadRequest(views.html.index(Contact.allContacts(), errors)),
+	    name => {
+	      Contact.create(name)
+	      Redirect(routes.Application.list)
+	    }
+	  )
+	}
 }
