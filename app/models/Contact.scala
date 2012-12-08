d958e4e85656504fc368665cf4e72abf90b83593
Sergio Rodriguez <rodriguezvalencia@gmail.com>
2012-12-08 13:01:35 +0000

    Models

---
+++ app/models/Contact.scala
@@ -0,0 +1,12 @@
+package models
+
+case class Contact(name: String)
+
+object Contact {
+  def allContacts(): List[Contact] = Nil
+
+  def create(name: String) = Nil
+
+  def delete(name: String) = Nil
+
+}
---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,11 @@
+# Tasks schema
+ 
+# --- !Ups
+
+CREATE TABLE contact (
+    name varchar(255)
+);
+ 
+# --- !Downs
+ 
+DROP TABLE contact;
