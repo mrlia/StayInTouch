1b2c93b41df6d9085358153845b468fb1cb4fcd0
Sergio Rodriguez <rodriguezvalencia@gmail.com>
2012-12-08 13:14:22 +0000

    get all contacts + create contacts methods

--- app/models/Contact.scala
+++ app/models/Contact.scala
@@ -1,11 +1,31 @@
 package models
 
+import play.api.db._
+import play.api.Play.current
+import anorm._
+import anorm.SqlParser._
+
 case class Contact(name: String)
 
-object Contact {
-  def allContacts(): List[Contact] = Nil
 
-  def create(name: String) = Nil
+
+object Contact {
+  val newContact = {
+    get[String] ("name") map {
+      case name => Contact(name)
+    }
+  }
+
+  def allContacts(): List[Contact] = DB.withConnection { implicit c =>
+    SQL("select * from contact").as(newContact *)
+  }
+
+  def create(name: String) = DB.withConnection { implicit c =>
+    SQL("insert into contact(name) values ({newName})").on(
+      'newName -> name
+    ).executeUpdate()
+  }
+  
 
   def delete(name: String) = Nil
 
